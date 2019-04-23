624efef8add07bbd99afc71a9471057d868a05f6
devgis <devgis@163.com>
2019-04-22 23:53:38 -0700

    TestRSA
    TestRSA

---
+++ TestRSA/Form1.Designer.cs
@@ -0,0 +1,261 @@
+﻿namespace TestRSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.button6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(134, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(220, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "生成RSA公钥和私钥";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "要加密的字符串:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 81);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 12);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "加密后的字符串:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(104, 55);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(250, 21);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(104, 79);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(250, 72);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 157);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(95, 12);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "解密后的字符串:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(104, 154);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(250, 21);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(360, 55);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "RSA加密";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(360, 153);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "RSA解密";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 184);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(95, 12);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "SHA1加密字符串:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(104, 178);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(250, 21);
+            this.textBox4.TabIndex = 4;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(360, 79);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 5;
+            this.button4.Text = "SHA1加密";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(104, 215);
+            this.textBox5.Multiline = true;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(250, 72);
+            this.textBox5.TabIndex = 6;
+            // 
+            // label5
+            // 
+            this.label5.Location = new System.Drawing.Point(14, 215);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 40);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "对SHA1密文签名后的字符串:";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(360, 264);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "RSA签名";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(39, 305);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(59, 12);
+            this.label6.TabIndex = 9;
+            this.label6.Text = "验证签名:";
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(104, 302);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(250, 21);
+            this.textBox6.TabIndex = 10;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(360, 300);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 11;
+            this.button6.Text = "RSA验签";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 371);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "RSA加密 解密 签名 验签";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ TestRSA/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace TestRSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 生成RSA公钥和私钥
+        /// </summary>
+        private void CreateRSAKey()
+        {
+            try
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                //生成私有密钥,这个文件是要保密的
+                using (StreamWriter writer = new StreamWriter("PrivateKey.xml"))
+                {
+                    writer.WriteLine(rsa.ToXmlString(true));
+                    writer.Close();
+                }
+
+                //生成公有密钥,可以公开
+                using (StreamWriter writer = new StreamWriter("PublicKey.xml"))
+                {
+                    writer.WriteLine(rsa.ToXmlString(false));
+                    writer.Close();
+                }
+                //这个可以没有
+                MessageBox.Show("公有密钥和私有密钥生成成功!", "提示", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        /// <summary>
+        /// RSA加密(使用公有密钥进行加密)
+        /// </summary>
+        /// <param name="privatekey">私有密钥</param>
+        /// <param name="content">要加密的字符串</param>
+        /// <returns>加密后的字符串</returns>
+        public static string RSAEncrypt(string publickey, string content)
+        {
+
+            RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+            byte[] cipherbytes;
+            rsa.FromXmlString(publickey);
+            cipherbytes = rsa.Encrypt(Encoding.UTF8.GetBytes(content), false);
+            return Convert.ToBase64String(cipherbytes);
+
+        }
+
+        /// <summary>
+        /// RSA解密
+        /// </summary>
+        /// <param name="publickey">私有密钥</param>
+        /// <param name="content">私有密钥加密过的字符串</param>
+        /// <returns>解密后的字符串</returns>
+        public static string RSADecrypt(string privatekey, string content)
+        {
+            try
+            {
+                RSACryptoServiceProvider rsa = new RSACryptoServiceProvider();
+                byte[] cipherbytes;
+                rsa.FromXmlString(privatekey);
+                cipherbytes = rsa.Decrypt(Convert.FromBase64String(content), false);
+                return Encoding.UTF8.GetString(cipherbytes);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return "";
+            }
+
+        }
+
+        /// <summary>
+        /// 对字符串进行SHA1加密
+        /// </summary>
+        /// <param name="str_source">原字符串</param>
+        /// <returns>加密后的字符串</returns>
+        public static string getHash(string str_source)
+        {
+            HashAlgorithm ha = HashAlgorithm.Create("MD5");
+            byte[] bytes = Encoding.GetEncoding(0).GetBytes(str_source);
+            byte[] str_hash = ha.ComputeHash(bytes);
+            return Convert.ToBase64String(str_hash);
+        }
+
+        /// <summary>
+        /// 对SHA1加密后的字符串进行RSA签名
+        /// </summary>
+        /// <param name="privatekey">私有密钥</param>
+        /// <param name="str_HashbyteSingture">SHA1加密后的字符串</param>
+        /// <returns>签名后的数据</returns>
+        public string SignatureFormatter(string privatekey, string str_HashbyteSingture)
+        {
+            byte[] rgbHash = Convert.FromBase64String(str_HashbyteSingture);
+            RSACryptoServiceProvider key = new RSACryptoServiceProvider();
+            key.FromXmlString(privatekey);
+            RSAPKCS1SignatureFormatter rsa_formatter = new RSAPKCS1SignatureFormatter(key);
+            rsa_formatter.SetHashAlgorithm("MD5");
+            byte[] bytes = rsa_formatter.CreateSignature(rgbHash);
+            return Convert.ToBase64String(bytes);
+        }
+
+        /// <summary>
+        /// 对RSA签名进行验证
+        /// </summary>
+        /// <param name="publickey">公有密钥</param>
+        /// <param name="strHashbyteDeformatter">SHA1加密后的字符串</param>
+        /// <param name="strDeformatterData">RSA签名后的字符串</param>
+        /// <returns></returns>
+        public bool SignatureDeformatter(string publickey, string strHashbyteDeformatter, string strDeformatterData)
+        {
+            byte[] rgbHash = Convert.FromBase64String(strHashbyteDeformatter);
+            RSACryptoServiceProvider key = new RSACryptoServiceProvider();
+            key.FromXmlString(publickey);
+            RSAPKCS1SignatureDeformatter rsa_Deformatter = new RSAPKCS1SignatureDeformatter(key);
+            rsa_Deformatter.SetHashAlgorithm("MD5");
+            byte[] rgbSignature = Convert.FromBase64String(strDeformatterData);
+            if (rsa_Deformatter.VerifySignature(rgbHash, rgbSignature))
+            {
+                return true;
+            }
+            return false;
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CreateRSAKey();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //读取公有密钥 进行加密
+            StreamReader sr = new StreamReader(@"PublicKey.xml");
+            string publicKey = sr.ReadLine();
+            sr.Close();
+            this.textBox2.Text = RSAEncrypt(publicKey, this.textBox1.Text);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            //读取私有密钥 进行解密
+            StreamReader sr = new StreamReader(@"PrivateKey.xml");
+            string privateKey = sr.ReadLine();
+            sr.Close();
+            this.textBox3.Text = RSADecrypt(privateKey, this.textBox2.Text);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //SHA1加密
+            this.textBox4.Text = getHash(this.textBox1.Text);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            //读取私有密钥 进行签名
+            StreamReader sr = new StreamReader(@"PrivateKey.xml");
+            string privatekey = sr.ReadLine();
+            sr.Close();
+            this.textBox5.Text = this.SignatureFormatter(privatekey, this.textBox4.Text);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader(@"PublicKey.xml");
+            string publickey = sr.ReadLine();
+            sr.Close();
+            this.textBox6.Text = SignatureDeformatter(publickey, this.textBox4.Text, textBox5.Text).ToString();
+        }
+    }
+}
